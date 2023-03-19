97012c4e05dcf3cc51b122a62f49ab6c19662622
LT&C <gemma@ltnc.nl>
2023-03-19 19:35:35 +0100

    Update version.php

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version = 2023030700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2017111300;        // Requires this Moodle version 3.4.
 $plugin->component = 'block_user_favorites';// Full name of the plugin (used for diagnostics).
-$plugin->release = '3.10.2';
+$plugin->release = '4.1.1';
 $plugin->maturity = MATURITY_STABLE;
